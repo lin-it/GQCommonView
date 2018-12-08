3b04743f5ae9ff0996f2020415ce6979a4b51f28
guoqiang.lin <guoqiang.lin@husor.com>
2018-12-08 11:16:22 +0800

    add file

--- GQCommonView/Classes/GQPhotoBrowserManager.m
+++ GQCommonView/Classes/GQPhotoBrowserManager.m
@@ -6,6 +6,47 @@
 //
 #import <GQTool/GQLazyProperty.h>
 #import "GQPhotoBrowserManager.h"
+#import <libextobjc/EXTScope.h>
+
+UIImage *GQPhotoBrowerImageFromView(UIView *v);
+
+UIImage *GQPhotoBrowerImageFromView(UIView *v) {
+    if ([v isKindOfClass:[UIImageView class]]) {
+        UIImageView *img = (UIImageView *)v;
+        return img.image;
+    }
+    UIGraphicsBeginImageContextWithOptions(v.bounds.size, YES, 0);
+    if (!v.layer.backgroundColor || CGColorEqualToColor(v.layer.backgroundColor, [UIColor clearColor].CGColor)) {
+        v.layer.backgroundColor = [UIColor whiteColor].CGColor;
+    }
+    [v.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return viewImage;
+}
+
+@interface __GQPhotoAnimatingImageView : UIImageView
+@property(nonatomic, strong) CAShapeLayer *maskLayer;
+@property(nonatomic, assign) UIEdgeInsets insets;
+
+@property (nonatomic, copy) void(^animationBlock)(void);
+@property (nonatomic, copy) void(^complete)(void);
+
+@property (nonatomic, assign) CGRect starMaskFrame;
+@property (nonatomic, assign) CGRect endMaskFrame;
+
+@property (nonatomic, assign) CGRect starFrame;
+@property (nonatomic, assign) CGRect endFrame;
+
+@property (nonatomic, assign) CGFloat duration;
+
+
+- (void)showAnimation;
+- (void)showAnimationWithMaskLayer:(BOOL)maskLayer;
+- (void)showMaskLayerWithTime:(CGFloat)duration;
+
+@end
+
 
 @implementation GQPercentDrivenInteractiveTransition
 
@@ -22,23 +63,65 @@
     UIViewController<GQPhotoBrowserManagerDelegate> *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
 
     UIView *containerView = [transitionContext containerView];
-    [containerView addSubview:toVC.view];
+//    [containerView addSubview:toVC.view];
 
     UIView *sourceView = nil;
     if ([toVC respondsToSelector:@selector(viewAtIdxBlock)] && [toVC respondsToSelector:@selector(currentIndex)]) {
         sourceView = toVC.viewAtIdxBlock(toVC.currentIndex);
     }
 
-    CGRect frame = [UIScreen mainScreen].bounds;
+    CGRect initialImageViewFrame = [sourceView.superview convertRect:sourceView.frame
+                                                              toView:[[UIApplication sharedApplication].delegate window]];
+
+    CGRect finallyFrame = [UIScreen mainScreen].bounds;
     if ([toVC respondsToSelector:@selector(toFrameAtIdxBlock)] && [toVC respondsToSelector:@selector(currentIndex)]) {
-        frame = toVC.toFrameAtIdxBlock(toVC.currentIndex);
+        finallyFrame = toVC.toFrameAtIdxBlock(toVC.currentIndex);
     }
+     __GQPhotoAnimatingImageView *animatingImageView = [[__GQPhotoAnimatingImageView alloc] initWithFrame:initialImageViewFrame];
+    CGRect starMaskRect = CGRectMake(0, 0, initialImageViewFrame.size.width, initialImageViewFrame.size.height);
+    CGRect starFrame = initialImageViewFrame;
+
+    UIImage *image = GQPhotoBrowerImageFromView(sourceView);
+    CGSize imageSize = image.size;
+    CGFloat w = 0;
+    CGFloat h = 0;
+    if (imageSize.width < imageSize.height) {
+        CGFloat r = imageSize.width / initialImageViewFrame.size.width;
+        h = imageSize.height / r;
+        w = initialImageViewFrame.size.width;
+    } else {
+        CGFloat r = imageSize.height / initialImageViewFrame.size.height;
+        w = imageSize.width / r;
+        h = initialImageViewFrame.size.height;
+    }
+    CGPoint center = animatingImageView.center;
+    starFrame = CGRectMake(center.x - w/2.f, center.y - h/2.f, w, h);
+    animatingImageView.frame = starFrame;
+    starMaskRect = CGRectMake((w - initialImageViewFrame.size.width)/2.f, (h - initialImageViewFrame.size.height)/2.f, initialImageViewFrame.size.width, initialImageViewFrame.size.height);
+
+    [containerView addSubview:animatingImageView];
+//    toVC.view.alpha = 0.f;
 
     NSTimeInterval duration = [self transitionDuration:transitionContext];
-    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
-    } completion:^(BOOL finished) {
+    animatingImageView.duration = duration;
+    animatingImageView.starMaskFrame = starMaskRect;
+    animatingImageView.starFrame = starFrame;
+    animatingImageView.endFrame = finallyFrame;
+    animatingImageView.endMaskFrame = CGRectMake(0, 0, finallyFrame.size.width, finallyFrame.size.height);
+    @weakify(toVC,animatingImageView,transitionContext,sourceView)
+    animatingImageView.animationBlock = ^{
+        @strongify(toVC)
+//        toVC.view.alpha = 1.f;
+    };
+    animatingImageView.complete = ^{
+        @strongify(toVC,animatingImageView,transitionContext,sourceView)
         [transitionContext completeTransition:YES];
-    }];
+        sourceView.hidden = NO;
+//        [toVC setBigImgViewHide:NO];
+//        [toVC reloadReplaceView];
+        [animatingImageView removeFromSuperview];
+    };
+    [animatingImageView showAnimation];
 }
 
 @end
@@ -106,4 +189,55 @@ GQLazyPropertyWithInit(GQPhotoBrowserDismissalAnimation, dismissalAnimation, {
 
 })
 
+@end
+
+@implementation __GQPhotoAnimatingImageView
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.contentMode = UIViewContentModeScaleAspectFit;
+        self.maskLayer = [CAShapeLayer layer];
+        self.maskLayer.backgroundColor = [UIColor whiteColor].CGColor;
+        self.layer.mask = self.maskLayer;
+        self.maskLayer.path = [UIBezierPath bezierPathWithRect:self.bounds].CGPath;
+    }
+    return self;
+}
+
+- (void)showMaskLayerWithTime:(CGFloat)duration {
+    CABasicAnimation *maskAnimation = [CABasicAnimation animationWithKeyPath:@"path"];
+    maskAnimation.fromValue = (__bridge id)([UIBezierPath bezierPathWithRect:self.starMaskFrame].CGPath);
+    maskAnimation.toValue = (__bridge id)([UIBezierPath bezierPathWithRect:CGRectMake(self.endMaskFrame.origin.x + self.insets.left, self.endMaskFrame.origin.y + self.insets.top, self.endMaskFrame.size.width - self.insets.left - self.insets.right, self.endMaskFrame.size.height - self.insets.bottom - self.insets.top)].CGPath);
+    maskAnimation.duration = duration;
+    maskAnimation.removedOnCompletion = NO;
+    maskAnimation.fillMode = kCAFillModeForwards;
+    maskAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    [self.maskLayer addAnimation:maskAnimation forKey:@"maskAnimation"];
+}
+
+- (void)showAnimationWithMaskLayer:(BOOL)maskLayer {
+
+    if (maskLayer && !CGRectEqualToRect(CGRectZero, self.endMaskFrame)) {
+        [self showMaskLayerWithTime:self.duration];
+    }
+    [UIView animateWithDuration:self.duration animations:^{
+        if (!CGRectEqualToRect(CGRectZero, self.endFrame)) {
+            self.frame = self.endFrame;
+        } else {
+            self.frame = CGRectMake(self.starFrame.origin.x, self.endFrame.origin.y + CGRectGetHeight([UIScreen mainScreen].bounds), CGRectGetWidth(self.frame), CGRectGetHeight(self.frame));
+        }
+        if (self.animationBlock) {
+            self.animationBlock();
+        }
+    } completion:^(BOOL finished) {
+        if (self.complete) {
+            self.complete();
+        }
+    }];
+}
+
+- (void)showAnimation {
+    [self showAnimationWithMaskLayer:YES];
+
+}
 @end
